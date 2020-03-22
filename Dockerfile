bc4f4177881ea66deefa739e58a3fc74350fa8e4
pcx <ddxgz19880813@gmail.com>
2020-03-22 12:49:01 +0100

    Add service script and update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,23 @@
 FROM ubuntu:19.04
+# FROM ubuntu:18.04
 
 # backup old sources.list
 #RUN mv /etc/apt/sources.list /etc/apt/sources.list.bk
 
 #ADD sources.list /etc/apt/sources.list
-
+WORKDIR /code
+COPY . /code
+# RUN apk add --no-cache gcc musl-dev linux-headers make automake g++ subversion python3-dev
 RUN apt-get -qqy update && apt-get install -y \
     python3 \
     python3-dev \
-    python3-pip 
+    python3-pip  && \ 
+    rm -rf /var/lib/apt/lists/*
 
-# RUN mkdir /code
-ADD . /code
-# ADD ../requirements.txt /code/
+COPY requirements.txt requirements.txt
+# RUN python -m pip install -U --force-reinstall pip
+RUN pip3 install -r requirements.txt --no-cache-dir
 
-# for copy requirements.txt to container
-# COPY . /server/docker-compose
+# CMD gunicorn -b 0.0.0.0:8070 -k gevent webapp:wsgiapp
 
-WORKDIR /code
-
-RUN pip3 install -r requirements.txt
-
-CMD gunicorn -b 0.0.0.0:8070 -k gevent webapp:wsgiapp
+ENTRYPOINT gunicorn -b 0.0.0.0:80 -k gevent webapp:wsgiapp
---
+++ service.py
@@ -0,0 +1,98 @@
+# This script is used as CLI for run/build/deploy this service
+
+import subprocess
+
+import click
+
+from webapp import app
+
+
+PROJECT_ID = "data-science-258408"
+IMAGE_NAME = "linkedinfo-ml-model"
+REGION = "us-central1"
+IMAGE_BUILD = f"gcr.io/{PROJECT_ID}/{IMAGE_NAME}"
+SERVICE_NAME = "linkedinfo-tag-pred"
+MEM_SIZE = '1024'
+CLOUD_PORT = '80'
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option('--local/--container', default=True)
+def run(local: bool):
+    if local:
+        app.debug = True
+        # app.run(host='0.0.0.0', threaded=True)
+        app.run(host='0.0.0.0')
+    else:
+        run_container()
+
+#         docker build --tag $ImageBuild . && docker run -it -p 5000:80 $ImageBuild
+# gcloud builds submit --tag gcr.io/$ProjectID/$ImageName
+@cli.command()
+@click.option('--local/--cloud', default=True)
+@click.option('--run', is_flag=True, default=False)
+@click.option('--deploy', is_flag=True, default=False)
+def build(local: bool, run: bool, deploy: bool):
+    if local:
+        build_local()
+    else:
+        build_gcloud()
+
+    if run:
+        run_container()
+
+    if deploy:
+        deploy_gcloud()
+
+
+#        docker push $ImageBuild
+#        gcloud run deploy $ServiceName --platform managed --region $Region --image $ImageBuild
+@cli.command()
+def deploy():
+    deploy_gcloud()
+
+
+def run_container():
+    print('Running image')
+    subprocess.run(['docker', 'run', '-it', '-p', '5000:80', IMAGE_BUILD])
+
+
+def build_local():
+    print('Building image locally')
+    subprocess.run(['docker', 'build', '--tag', IMAGE_BUILD, '.'])
+
+
+def build_gcloud():
+    print('Building image in Google Cloud Build')
+    subprocess.run(['gcloud', 'builds', 'submit',
+                    '--tag', IMAGE_BUILD])
+
+
+def deploy_gcloud():
+    print('Deploying image')
+    subprocess.run(['docker', 'push', IMAGE_BUILD])
+    subprocess.run(['gcloud', 'run', 'deploy',
+                    SERVICE_NAME, '--platform', 'managed', '--region', REGION,
+                    '--image', IMAGE_BUILD, '--memory', MEM_SIZE, '--port',
+                    CLOUD_PORT])
+
+
+# MODEL_PATH = "models"
+# BUCKET = "data-science-258408-skin-lesion-cls-models"
+# MODEL_CLOUD = "models/dense161.pth.tar"
+#
+# # gsutil cp $ModelPath gs://$Bucket/$ModelCloud
+# # https://storage.cloud.google.com/data-science-258408-skin-lesion-cls-models/models/dense161.pth.tar
+# @cli.command()
+# @click.argument('model', type=click.Path(exists=True))
+# def upload(model: str):
+#     pass
+
+
+if __name__ == '__main__':
+    cli()
