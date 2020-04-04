5ad7fc78c03a7a94982ab5c7d6e55c0916463b59
pcx <ddxgz19880813@gmail.com>
2020-04-04 16:41:37 +0200

    Update utils

--- mltb/metrics.py
+++ mltb/metrics.py
@@ -1,10 +1,16 @@
 import pandas as pd
 from sklearn import metrics
+from typing import List
 
 
-def classification_report_avg(y_true, y_pred):
+def classification_report_avg(y_true, y_pred, cols_avg: List[str] = None):
     report = metrics.classification_report(
-        y_true, y_pred, output_dict=True)
+        y_true, y_pred, output_dict=True, zero_division=0)
     df_report = pd.DataFrame(report).transpose()
-    cols_avg = ['micro avg', 'macro avg', 'weighted avg', 'samples avg']
-    return df_report.loc[cols_avg]
+
+    if cols_avg is not None:
+        cols = cols_avg
+    else:
+        cols = ['micro avg', 'macro avg', 'weighted avg', 'samples avg']
+
+    return df_report.loc[cols, ]
