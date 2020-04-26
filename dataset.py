95b4eb9152ee5533c35637d9e59865d43e56997d
pcx <ddxgz19880813@gmail.com>
2020-04-26 16:28:46 +0200

    Add stackoverflow posts dataset

--- dataset.py
+++ dataset.py
@@ -26,7 +26,7 @@ from sklearn.model_selection import train_test_split
 # import nltk
 # from nltk.tokenize.treebank import TreebankWordTokenizer,
 # TreebankWordDetokenizer
-import spacy
+# import spacy
 # import pysnooper
 from typing import List, Callable
 
@@ -34,7 +34,7 @@ import extractor
 
 
 # nltk.download('punkt')
-nlp = spacy.load('en_core_web_sm')
+# nlp = spacy.load('en_core_web_sm')
 
 
 logger = logging.getLogger('dataset')
@@ -60,6 +60,12 @@ LAN_ENCODING = {
     'cn': 1,
 }
 
+STACKFILES = [
+    '/content/data/stackoverflow/stackoverflow-top50k.csv',
+    '/content/data/stackoverflow/stackoverflow-score81-47-50k.csv',
+    '/content/data/stackoverflow/stackoverflow-score46-33-50k.csv',
+    '/content/data/stackoverflow/stackoverflow-score32-25-50k.csv',
+]
 
 # TODO: maybe change dataset to namedtuple
 @dataclass
@@ -88,6 +94,15 @@ class DataappSet:
         with open(filename, 'wb')as f:
             pickle.dump(self, f, pickle.HIGHEST_PROTOCOL)
         return filename
+
+
+@dataclass
+class DatasetStack:
+    data: pd.DataFrame
+    target: pd.DataFrame
+    target_names: pd.DataFrame
+    target_decoded: pd.DataFrame
+    mlb: MultiLabelBinarizer
 
 
 def clean_text(text):
@@ -571,3 +586,47 @@ def df_lan(*args, **kwargs):
     #     data_lst = list()
 
     return df
+
+
+def ds_stack(stackfiles: List[str] = STACKFILES, text_cols: List[str] = [], name_text_col: str = 'description',
+             n_tag: int = 500, concat_title: bool = False, body_extractor: Callable = None):
+    from bs4 import BeautifulSoup
+
+    df = pd.concat([pd.read_csv(f) for f in stackfiles], ignore_index=True)
+
+    tags_all = df['Tags'].str.extractall(r'\<([^\>]+)\>')
+
+    tags = tags_all.unstack()
+    dfc = tags_all[0].value_counts()[:500]
+
+    tags_valid = tags_all[tags_all[0].isin(dfc.index)]
+    tags_valid.xs(0, level='match')
+    idx_keep = tags_valid.xs(0, level='match').index
+    df_keep = df.iloc[idx_keep]
+
+    tags_keep = tags_valid.iloc[idx_keep]
+    tags_withnan = tags_keep.values.tolist()
+
+    def not_nan(lst):
+        return [tag for tag in lst if str(tag) != 'nan']
+
+    tags_lst = map(not_nan, tags_withnan)
+
+    mlb = MultiLabelBinarizer()
+    Y = mlb.fit_transform(tags_lst)
+
+    def extract_stack_body_simple(rec):
+        soup = BeautifulSoup(rec, 'html.parser')
+
+        return ''.join(soup.strings).replace('\n', ' ')
+
+    if body_extractor is None:
+        body_extractor = extract_stack_body_simple
+
+    df_keep['body'] = df_keep['body'].apply(body_extractor)
+
+    if concat_title:
+        df_keep[name_text_col] = df_keep['Title'].str.cat(
+            df_keep['body'], sep=' ')
+
+    return DatasetStack(df_keep, Y, mlb.classes_, tags_lst, mlb)
