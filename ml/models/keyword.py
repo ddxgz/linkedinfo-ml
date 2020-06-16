41aa76f394830430bbd29c15ef810aaef3ed480a
pcx <ddxgz19880813@gmail.com>
2020-06-16 17:42:32 +0200

    Temporary fix keywords index error

--- ml/models/keyword.py
+++ ml/models/keyword.py
@@ -8,8 +8,18 @@ from .base import singleton
 class KeywordPredictor(object):
 
     def predict(self, text, num_keywords) -> List[str]:
-        raw_keywords = keywords(
-            text, scores=True, lemmatize=True, words=num_keywords)
+        try:
+            raw_keywords = keywords(
+                text, scores=True, lemmatize=True, words=num_keywords)
+        # REVIEW: need further wordaround
+        except IndexError as e:
+            # num_keywords -= 2
+            num_keywords = 1
+            try:
+                raw_keywords = keywords(
+                    text, scores=True, lemmatize=True, words=num_keywords)
+            except IndexError:
+                return []
 
         words = [pair[0] for pair in raw_keywords]
         return words
