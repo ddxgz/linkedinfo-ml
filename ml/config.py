1d5525b865c61c4322e0ed83ce8e330762d7609b
pcx <ddxgz19880813@gmail.com>
2020-08-07 11:10:18 +0200

    Add model_path to config

--- ml/config.py
+++ ml/config.py
@@ -3,20 +3,23 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
+
+ENV_PREFIX = 'LINKEDINFO_'
+
 # load_dotenv does not override existing System environment variables.
 load_dotenv()
 
-env = os.getenv('LINKEDINFO_ENV')
-if env is None:
-    env = 'prod'
+env = os.getenv(f'{ENV_PREFIX}ENV', default='prod')
 
 test_model = False
-test_model_env = os.getenv('LINKEDINFO_TEST_MODEL')
-if test_model_env is not None and test_model_env not in ('False', 'false', '0'):
+test_model_env = os.getenv(f'{ENV_PREFIX}TEST_MODEL')
+if env != 'prod' and test_model_env is not None and test_model_env not in ('False', 'false', '0'):
     test_model = True
 
-log_level = os.getenv('LINKEDINFO_LOG_LEVEL')
-if log_level is None:
-    log_level = 'error'
+log_level = os.getenv(f'{ENV_PREFIX}LOG_LEVEL', default='error')
 
-# print(test_model)
+model_path = os.getenv(f'{ENV_PREFIX}MODEL_PATH', default='data/models/')
+if not os.path.exists(model_path):
+    os.makedirs(model_path)
+
+# print(test_model, env, model_path)
